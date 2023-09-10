f8a4d5fe358cbe314fbc680544fe19541f19c341
coderrahim <coderrahimbd@gmail.com>
2023-09-10 08:17:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Countries from './components/Countries'
+
+function App() {
+
+  return (
+    <>      
+      <h2>REST Countries API</h2>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
